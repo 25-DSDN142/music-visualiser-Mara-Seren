e935dd1b7d2e9fb80aa2732d41297c328167088c
Mara-Seren <maraseren14@gmail.com>
2025-08-05 11:26:20 +1200

    Update music_starter.js
    Start on Project 2

--- music_starter.js
+++ music_starter.js
@@ -1,13 +1,15 @@
 
 let sunY =350;
 let sunSize;
+let bgCol
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
+  background(108, 123, bgCol)
 sunSize = map(drum, 0, 100,50, 400)
+bgCol = map(bass,0, 100,100, 255)
 
-  fill(255, 255, 0); // yellow
-
+  fill(206, 88, 0); // orange
+  
   ellipse(300,sunY,sunSize)
 sunY = sunY +1
 
